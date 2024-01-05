32d26933d4a837d03844a60f5bb32d9d90a82617
Valentin1302 <valentingautier8@gmail.com>
2024-01-05 11:22:34 +0100

    ecfback

--- views/plats/getAll.php
+++ views/plats/getAll.php
@@ -7,29 +7,34 @@ require_once('./models/plats/getAll.php');
 
 <h1>Gestion des plats du restaurant&nbsp;:</h1>
 <main>
-    <section>
 
-          <!-- Début de la boucle : pour chaque élément dans le tableau $results -->
+
+    <!-- Début de la boucle : pour chaque élément dans le tableau $results -->
+    <section class="blocSite">
         <?php foreach ($results as $single) { ?>
-            <div id="blocSite">
+            <div class="infoProduct">
+
                 <!-- Affichage des données à partir du tableau $single -->
                 <p>Nom du plat :<?php echo $single["name"] ?></p>
                 <p>Description :<?php echo $single["description"] ?></p>
                 <p>Prix :<?php echo $single["price"] ?></p>
                 <img src="<?= $single["img"]; ?>" alt="illustration de <?= $single["description"]; ?>">
-            
-                <div>
-                    <p><a href="../../views/crud/formUpdate.php?id=<?php echo $single["id"] ?>" title="Editer ce plat">📝</a></p>
-                </div>
-                <div>
-                    <p><a href="./models/plats/delete.php?id=<?php echo $single["id"]  ?>" title="Supprimer ce plat">🗑️</a></p>
-
-                </div>
             </div>
-        <?php } // endForeach 
-        ?>
+                <?php if (isset($_SESSION["connected"]) && $_SESSION["connected"] === TRUE) { ?>
+
+            <div>
+                <p><a href="../../views/crud/formUpdate.php?id=<?php echo $single["id"] ?>" title="Editer ce plat">📝</a></p>
+            </div>
+            <div>
+                <p><a href="./models/plats/delete.php?id=<?php echo $single["id"]  ?>" title="Supprimer ce plat">🗑️</a></p>
+
+            </div>
     </section>
-        <div class="blocAdd">
+
+
+    <div class="blocAdd">
         <p class="add"><a href="../../views/crud/formAddons.php" title="Ajouter un plat à la carte du restaurant">Ajouter un plat</a></p>
-        </div>
-    </main>
+    </div>
+<?php } ?>
+         <?php   } ?>
+</main>
